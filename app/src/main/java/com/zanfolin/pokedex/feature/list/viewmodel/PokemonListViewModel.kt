d2f604da825cd6da72e104642f82ed40ac8aa5ac
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2019-10-13 18:46:11 -0300

    Added LiveData attribute

--- app/src/main/java/com/zanfolin/pokedex/feature/list/viewmodel/PokemonListViewModel.kt
+++ app/src/main/java/com/zanfolin/pokedex/feature/list/viewmodel/PokemonListViewModel.kt
@@ -1,5 +1,7 @@
 package com.zanfolin.pokedex.feature.list.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.zanfolin.pokedex.base.model.pokemon.PokemonRepository
 import com.zanfolin.pokedex.base.domain.Pokemon
@@ -12,6 +14,8 @@ private const val PAGE_OFFSET = POKEMONS_PER_REQUEST - 1
 class PokemonListViewModel(region: Region, val repository: PokemonRepository) : ViewModel() {
 
     private var nextPokemonId = PokemonNationalDexIdProvider.getFirstByRegion(region)
+
+    private val data: MutableLiveData<List<Pokemon>> by lazy { MutableLiveData() }
 
     fun getMorePokemons(onResult: (Pokemon) -> Unit, onError: (Int, Throwable) -> Unit) {
         val lastIdToSearch = nextPokemonId + PAGE_OFFSET
