851808736d7e8d88ae1ad1f07eaa392d44247321
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2019-10-26 11:11:56 -0300

    Added service hosts enum

---
+++ app/src/main/java/com/zanfolin/pokedex/base/service/Hosts.kt
@@ -0,0 +1,11 @@
+package com.zanfolin.pokedex.base.service
+
+import okhttp3.HttpUrl
+
+enum class Hosts(private val url: String) {
+
+    POKE_API("https://pokeapi.co/api/v2/");
+
+    fun getHost() = HttpUrl.get(url)
+
+}
