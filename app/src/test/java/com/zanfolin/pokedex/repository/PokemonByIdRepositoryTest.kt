2ee252a338f3a0cee4c9db47faa4653c6ed464f8
Matheus A. Zanfolin <matheus.zanfolin@dextra-sw.com>
2019-10-26 12:35:15 -0300

    Stubbed first test

--- app/src/test/java/com/zanfolin/pokedex/repository/PokemonByIdRepositoryTest.kt
+++ app/src/test/java/com/zanfolin/pokedex/repository/PokemonByIdRepositoryTest.kt
@@ -2,22 +2,33 @@ package com.zanfolin.pokedex.repository
 
 import com.zanfolin.pokedex.base.model.api.pokeapi.PokeAPIConfiguration
 import com.zanfolin.pokedex.base.model.repository.pokemon.name.PokemonByIdRepository
+import com.zanfolin.pokedex.base.util.FileUtils
+import com.zanfolin.pokedex.base.util.FileUtils.readJsonForEndpoint
 import com.zanfolin.pokedex.repository.base.MockedAPI
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import java.net.HttpURLConnection.HTTP_OK
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.android.plugins.RxAndroidPlugins.setInitMainThreadSchedulerHandler
+
+
+private const val TEST_ID = 1
 
 class PokemonByIdRepositoryTest {
+
+    private val POKEMON_BY_ID_ENDPOINT = "pokemon/$TEST_ID"
 
     private val mockServer = MockWebServer()
 
     private val repository = PokemonByIdRepository(MockedAPI(PokeAPIConfiguration(), mockServer))
 
     @Before
-    fun setup() = mockServer.start()
+    fun setup() = setInitMainThreadSchedulerHandler { Schedulers.trampoline() };
 
     @After
     fun teardown() = mockServer.shutdown()
@@ -26,12 +37,15 @@ class PokemonByIdRepositoryTest {
     fun `when requesting pokemon by ID should return Pokemon`() {
         mockServer.enqueue(MockResponse().apply {
             setResponseCode(HTTP_OK)
-            setBody("") // TODO Get body from file
+            setBody(readJsonForEndpoint(POKEMON_BY_ID_ENDPOINT))
         })
 
-        val pkmn = repository.getPokemonById(1).blockingGet() // TODO Add mock for Bulbasaur
+        val pkmn = repository.getPokemonById(TEST_ID).blockingGet()
 
-        // TODO Perform assertions
+        assertEquals(1, pkmn.id)
+        assertEquals("bulbasaur", pkmn.name)
+        assertEquals(7, pkmn.height)
+        assertEquals(69, pkmn.weight)
     }
 
 }
